3164367e38fc02cd8669e5489b60865d7b085f4d
Gregory Morgan <gregorymorgan2@yahoo.com>
2018-08-01 19:30:29 -0700

    fourth one

--- src/pokecard.js
+++ src/pokecard.js
@@ -7,6 +7,8 @@ class PokeCard extends React.PureComponent {
       <div>
         <p>
           <span>#{this.props.number}</span> <span>{this.props.name}</span>
+          <br />
+          <span>{this.props.url}</span>
         </p>
       </div>
     );
--- src/pokemon.js
+++ src/pokemon.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import PokeCard from "./pokecard.js";
 import PokemonSpinner from "./pokemonspinner.jsx";
+import "./pokemon.css";
 
 const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/?limit=949";
 
@@ -11,12 +12,44 @@ class PokemonCards extends React.Component {
     this.state = {
       pokemonList: []
     };
+
+    this.sort = this.sort.bind(this);
+    this.reverseSort = this.reverseSort.bind(this);
+    this.sortByOriginalOrder = this.sortByOriginalOrder.bind(this);
   }
 
   componentDidMount() {
     axios.get(pokemonUrl).then(data => {
-      this.setState({ pokemonList: data.data.results });
+      this.setState({
+        pokemonList: data.data.results
+      });
     });
+  }
+
+  sortByOriginalOrder() {
+    axios.get(pokemonUrl).then(data => {
+      this.setState({
+        pokemonList: data.data.results
+      });
+    });
+  }
+
+  sort() {
+    let sortedList = this.state.pokemonList.sort(function(a, b) {
+      if (a.name > b.name) return 1;
+      else if (a.name < b.name) return -1;
+      return 0;
+    });
+    this.setState({ pokemonList: sortedList });
+  }
+
+  reverseSort() {
+    let reverseSortedList = this.state.pokemonList.sort(function(a, b) {
+      if (a.name > b.name) return -1;
+      else if (a.name < b.name) return 1;
+      return 0;
+    });
+    this.setState({ pokemonList: reverseSortedList });
   }
 
   render() {
@@ -28,19 +61,36 @@ class PokemonCards extends React.Component {
               poke.name.substring(0, 1).toUpperCase() + poke.name.substring(1)
             }
             number={pokeIndex + 1}
+            // url={poke.url}
           />
         </li>
       </ol>
     ));
     return (
       <div>
-        <h2>List of all Pokemon:</h2>
         <img
           alt="Pokemon!"
           src="//upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/250px-International_Pok%C3%A9mon_logo.svg.png"
         />
         <br />
         <PokemonSpinner />
+        <br />
+        <h2>List of all Pokemon:</h2>
+        <br />
+        <button type="button" className="button" onClick={this.sort}>
+          Sort By Name
+        </button>
+        <button type="button" className="button" onClick={this.reverseSort}>
+          Sort By Reverse Name
+        </button>
+        <button
+          type="button"
+          className="button"
+          onClick={this.sortByOriginalOrder}
+        >
+          Reset List
+        </button>
+        <br />
         <br />
         {pokes}
       </div>
